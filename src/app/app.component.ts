e7280e3b39a0b93263118a979517cb279b8a566c
abiliojunior <abiliojunior.mat@gmail.com>
2023-12-20 22:14:17 -0300

    Feito a percepção dos checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'caves';
+  title = 'Gerador Caves';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AventureirosComponent } from './aventureiros/aventureiros.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AventureirosComponent } from './aventureiros/aventureiros.component';
     AventureirosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
